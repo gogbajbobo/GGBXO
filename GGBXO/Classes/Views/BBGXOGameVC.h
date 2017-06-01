18e7877e8bba80a72c7deeedb4b859b5de75c0f1
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-01 11:07:30 +0300

    add BBGXOGameVC

---
+++ GGBXO/Classes/Views/BBGXOGameVC.h
@@ -0,0 +1,13 @@
+//
+//  BBGXOGameVC.h
+//  GGBXO
+//
+//  Created by Maxim Grigoriev on 01/06/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBGXOGameVC : UIViewController
+
+@end
---
+++ GGBXO/Classes/Views/BBGXOGameVC.m
@@ -0,0 +1,37 @@
+//
+//  BBGXOGameVC.m
+//  GGBXO
+//
+//  Created by Maxim Grigoriev on 01/06/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import "BBGXOGameVC.h"
+
+@interface BBGXOGameVC ()
+
+@end
+
+@implementation BBGXOGameVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
