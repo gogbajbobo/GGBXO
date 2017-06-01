6a18bd9cd57d78309f8c5131149c39929f21f0ab
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-01 11:11:14 +0300

    add GGBXOMenuVC

---
+++ GGBXO/Classes/Views/GGBXOMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  GGBXOMenuVC.h
+//  GGBXO
+//
+//  Created by Maxim Grigoriev on 01/06/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGBXOMenuVC : UIViewController
+
+@end
