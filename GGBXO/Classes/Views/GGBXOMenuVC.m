11a4d76c4f7480eedb234cd4fcf104bee8eca43c
gogbajbobo <grigoriev.maxim@gmail.com>
2017-06-01 11:27:01 +0300

    prepareForSegue: in GGBXOMenuVC

--- GGBXO/Classes/Views/GGBXOMenuVC.m
+++ GGBXO/Classes/Views/GGBXOMenuVC.m
@@ -8,6 +8,9 @@
 
 #import "GGBXOMenuVC.h"
 
+#import "GGBXOGameVC.h"
+
+
 @interface GGBXOMenuVC ()
 
 @end
@@ -24,14 +27,17 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+    if (![segue.destinationViewController isKindOfClass:[GGBXOGameVC class]]) return;
+    
+    GGBXOGameVC *gameVC = (GGBXOGameVC *)segue.destinationViewController;
+    gameVC.playVsAI = [segue.identifier isEqualToString:@"playVsAI"];
+    
 }
-*/
+
 
 @end
